3d66bf928ee805e46c3ad30d9378016d39fae12b
Adimchi <oraegbunamadimchi@gmail.com>
2022-03-30 09:56:45 +0100

    Changed city to Toronto and added relevant icon

--- src/app.js
+++ src/app.js
@@ -1,21 +1,59 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
 
 function displayWeather(response) {
-    let city= document.querySelector("#city");
-   let description = document.querySelector("#description");
-    let temperature = document.querySelector("#temperature");
-    let humidity = document.querySelector("#humidity");
-    let wind = document.querySelector("#wind");
-        city.innerHTML = response.data.name;
-        description.innerHTML =response.data.weather[0].description;
-temperature.innerHTML = Math.round(response.data.main.temp);
-humidity.innerHTML= response.data.main.humidity;
-wind.innerHTML=Math.round( response.data.wind.speed);
-   
+  let city = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let temperature = document.querySelector("#temperature");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let date = document.querySelector("#date");
+  let icon= document.querySelector("#icon");
+  city.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  date.innerHTML =  formatDate(response.data.dt * 1000);
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+   icon.setAttribute("alt", response.data.weather[0].description);
+
 }
 
 let apiKey = "0c249e7e69e1efdbbeab00f732a6ff42";
-let url =
-  `https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=${apiKey}&units=metric`;
-  
-  axios.get(url).then(displayWeather);
+let city = "Toronto";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(url).then(displayWeather);
